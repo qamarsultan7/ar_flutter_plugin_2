65014c1e3fd86a63e7e606891777fa4dbaa5eef4
Qamar Sultan <139233554+qamarsultan7@users.noreply.github.com>
2025-12-11 22:19:12 +0500

    Refactor ARSessionManager for improved clarity

--- lib/managers/ar_session_manager.dart
+++ lib/managers/ar_session_manager.dart
@@ -1,5 +1,5 @@
-import 'dart:math' show sqrt;
 import 'dart:typed_data';
+import 'dart:math' show sqrt;
 
 import 'package:ar_flutter_plugin_2/datatypes/config_planedetection.dart';
 import 'package:ar_flutter_plugin_2/models/ar_hittest_result.dart';
@@ -10,65 +10,120 @@ import 'package:vector_math/vector_math_64.dart';
 
 typedef ARHitResultHandler = void Function(List<ARHitTestResult> hits);
 typedef ErrorHandler = void Function(String error);
-typedef PlaneDetectedHandler = void Function(int count);
 
-/// Manages the session configuration, parameters and events of an ARView
+/// Dummy ARAnchor class definition for compatibility
+class ARAnchor {
+  final String name;
+  ARAnchor(this.name);
+}
+
+/// Manages the session configuration, parameters and events of an [ARView]
 class ARSessionManager {
+  /// Platform channel used for communication from and to [ARSessionManager]
   late MethodChannel _channel;
 
+  /// Debugging status flag. If true, all platform calls are printed. Defaults to false.
   final bool debug;
+
+  /// Context of the [ARView] widget that this manager is attributed to
   final BuildContext buildContext;
+
+  /// Determines the types of planes ARCore and ARKit should show
   final PlaneDetectionConfig planeDetectionConfig;
 
+  /// Receives hit results from user taps with tracked planes or feature points
   late ARHitResultHandler onPlaneOrPointTap;
 
-  /// Plane detection callback
-  PlaneDetectedHandler? onPlaneDetected;
+  /// Receives total number of Planes when a plane is detected and added to the view
+  void Function(int planeCount)? onPlaneDetected;
 
+  /// Callback that is triggered once error is triggered
   ErrorHandler? onError;
 
-  ARSessionManager(int id, this.buildContext, this.planeDetectionConfig,
-      {this.debug = false}) {
+  ARSessionManager(
+    int id,
+    this.buildContext,
+    this.planeDetectionConfig, {
+    this.debug = false,
+  }) {
     _channel = MethodChannel('arsession_$id');
     _channel.setMethodCallHandler(_platformCallHandler);
+    if (debug) {
+      print("ARSessionManager initialized");
+    }
   }
 
-  /// Returns camera pose
+  /// Returns the camera pose in Matrix4 format with respect to the world coordinate system of the [ARView]
   Future<Matrix4?> getCameraPose() async {
     try {
-      final serialized =
+      final serializedCameraPose =
           await _channel.invokeMethod<List<dynamic>>('getCameraPose', {});
-      return MatrixConverter().fromJson(serialized!);
+      return MatrixConverter().fromJson(serializedCameraPose!);
     } catch (e) {
-      print("Error: $e");
+      print('Error caught: ' + e.toString());
       return null;
     }
   }
 
-  /// Hit test screen plane
-  Future<List<Matrix4>> hitTestScreenPosition(double x, double y) async {
-    final List results = await _channel.invokeMethod("hitTest", {
-      "x": x,
-      "y": y,
-    });
-
-    if (results.isEmpty) {
-      debugPrint("⚠️ No hit test results found");
-      return [];
+  /// Returns the given anchor pose in Matrix4 format with respect to the world coordinate system of the [ARView]
+  Future<Matrix4?> getPose(ARAnchor anchor) async {
+    try {
+      if (anchor.name.isEmpty) {
+        throw Exception("Anchor can not be resolved. Anchor name is empty.");
+      }
+      final serializedCameraPose =
+          await _channel.invokeMethod<List<dynamic>>('getAnchorPose', {
+        "anchorId": anchor.name,
+      });
+      return MatrixConverter().fromJson(serializedCameraPose!);
+    } catch (e) {
+      print('Error caught: ' + e.toString());
+      return null;
     }
-
-    return results.map<Matrix4>((result) {
-      final List<dynamic> matrixValues = result["worldTransform"];
-      return Matrix4.fromList(matrixValues.cast<double>());
-    }).toList();
   }
 
-  // Disable camera
+  /// Returns the distance in meters between @anchor1 and @anchor2.
+  Future<double?> getDistanceBetweenAnchors(
+      ARAnchor anchor1, ARAnchor anchor2) async {
+    var anchor1Pose = await getPose(anchor1);
+    var anchor2Pose = await getPose(anchor2);
+    var anchor1Translation = anchor1Pose?.getTranslation();
+    var anchor2Translation = anchor2Pose?.getTranslation();
+    if (anchor1Translation != null && anchor2Translation != null) {
+      return getDistanceBetweenVectors(anchor1Translation, anchor2Translation);
+    } else {
+      return null;
+    }
+  }
+
+  /// Returns the distance in meters between @anchor and device's camera.
+  Future<double?> getDistanceFromAnchor(ARAnchor anchor) async {
+    Matrix4? cameraPose = await getCameraPose();
+    Matrix4? anchorPose = await getPose(anchor);
+    Vector3? cameraTranslation = cameraPose?.getTranslation();
+    Vector3? anchorTranslation = anchorPose?.getTranslation();
+    if (anchorTranslation != null && cameraTranslation != null) {
+      return getDistanceBetweenVectors(anchorTranslation, cameraTranslation);
+    } else {
+      return null;
+    }
+  }
+
+  /// Returns the distance in meters between @vector1 and @vector2.
+  double getDistanceBetweenVectors(Vector3 vector1, Vector3 vector2) {
+    num dx = vector1.x - vector2.x;
+    num dy = vector1.y - vector2.y;
+    num dz = vector1.z - vector2.z;
+    double distance = sqrt(dx * dx + dy * dy + dz * dz);
+    return distance;
+  }
+
+  // Disable Camera
   void disableCamera() {
     _channel.invokeMethod<void>('disableCamera');
   }
 
-  // Enable camera
+  // Enable Camera
   void enableCamera() {
     _channel.invokeMethod<void>('enableCamera');
   }
@@ -80,7 +135,30 @@ class ARSessionManager {
     });
   }
 
+  Future<List<Matrix4>> hitTestScreenPosition(double x, double y) async {
+    final List results = await _channel.invokeMethod("hitTest", {
+      "x": x,
+      "y": y,
+    });
+
+    if (results.isEmpty) {
+      debugPrint("⚠️ No hit test results from native");
+      return [];
+    }
+
+    debugPrint("📡 Raw hit test results from native: $results");
+
+    // Convert each result["worldTransform"] into Matrix4
+    return results.map<Matrix4>((result) {
+      final List<dynamic> matrixValues = result["worldTransform"];
+      return Matrix4.fromList(matrixValues.cast<double>());
+    }).toList();
+  }
+
   Future<void> _platformCallHandler(MethodCall call) {
+    if (debug) {
+      print('_platformCallHandler call ${call.method} ${call.arguments}');
+    }
     try {
       switch (call.method) {
         case 'onError':
@@ -92,39 +170,46 @@ class ARSessionManager {
                 content: Text(call.arguments[0]),
                 action: SnackBarAction(
                   label: 'HIDE',
-                  onPressed: () => ScaffoldMessenger.of(buildContext)
-                      .hideCurrentSnackBar(),
+                  onPressed: ScaffoldMessenger.of(buildContext)
+                      .hideCurrentSnackBar,
                 ),
               ),
             );
           }
           break;
-
         case 'onPlaneOrPointTap':
-          final raw = call.arguments as List<dynamic>;
-          final converted = raw
-              .map((e) => ARHitTestResult.fromJson(Map<String, dynamic>.from(e)))
-              .toList();
-          onPlaneOrPointTap(converted);
-          break;
-
-        case 'onPlaneDetected':
-          if (onPlaneDetected != null) {
-            onPlaneDetected!(call.arguments as int);
+          if (onPlaneOrPointTap != null) {
+            final rawHitTestResults = call.arguments as List<dynamic>;
+            final serializedHitTestResults = rawHitTestResults
+                .map((hitTestResult) => Map<String, dynamic>.from(hitTestResult))
+                .toList();
+            final hitTestResults = serializedHitTestResults
+                .map((e) => ARHitTestResult.fromJson(e))
+                .toList();
+            onPlaneOrPointTap(hitTestResults);
           }
           break;
-
+        case 'onPlaneDetected':
+          if (onPlaneDetected != null) {
+            final planeCountResult = call.arguments as int;
+            onPlaneDetected!(planeCountResult);
+          }
+          break;
+        case 'dispose':
+          _channel.invokeMethod<void>("dispose");
+          break;
         default:
-          if (debug) print("Unhandled method ${call.method}");
+          if (debug) {
+            print('Unimplemented method ${call.method} ');
+          }
       }
     } catch (e) {
-      print("Platform handler error: $e");
+      print('Error caught: ' + e.toString());
     }
-
     return Future.value();
   }
 
-  /// Initialize AR Session
+  /// Function to initialize the platform-specific AR view.
   onInitialize({
     bool showAnimatedGuide = true,
     bool showFeaturePoints = false,
@@ -135,7 +220,7 @@ class ARSessionManager {
     bool handlePans = false,
     bool handleRotation = false,
   }) {
-    _channel.invokeMethod("init", {
+    _channel.invokeMethod<void>('init', {
       'showAnimatedGuide': showAnimatedGuide,
       'showFeaturePoints': showFeaturePoints,
       'planeDetectionConfig': planeDetectionConfig.index,
@@ -148,18 +233,18 @@ class ARSessionManager {
     });
   }
 
-  /// Dispose session
+  /// Dispose the AR view on the platforms
   dispose() async {
     try {
-      await _channel.invokeMethod("dispose");
+      await _channel.invokeMethod<void>("dispose");
     } catch (e) {
-      print("Dispose error: $e");
+      print(e);
     }
   }
 
-  /// Take snapshot
+  /// Returns a future ImageProvider that contains a screenshot of the current AR Scene
   Future<ImageProvider> snapshot() async {
-    final bytes = await _channel.invokeMethod<Uint8List>('snapshot');
-    return MemoryImage(bytes!);
+    final result = await _channel.invokeMethod<Uint8List>('snapshot');
+    return MemoryImage(result!);
   }
 }
